5b13bd23855b19a0e0f970c9634ab65b119f9372
Adam Champion <championone@gmail.com>
2022-01-26 15:15:52 -0500

    Upgraded for Android Studio 2021.1.1 Bumblebee

--- app/src/main/kotlin/com/wiley/fordummies/androidsdk/tictactoe/model/QueryPreferences.kt
+++ app/src/main/kotlin/com/wiley/fordummies/androidsdk/tictactoe/model/QueryPreferences.kt
@@ -9,36 +9,36 @@ object QueryPreferences {
 	private const val PREF_LAST_RESULT_ID = "lastResultId"
 	private const val PREF_IS_POLLING = "isPolling"
 
-	fun getStoredQuery(context: Context?): String? {
+	fun getStoredQuery(context: Context): String? {
 		val prefs = PreferenceManager.getDefaultSharedPreferences(context)
 		return prefs.getString(PREF_SEARCH_QUERY, "")!!
 	}
 
-	fun setStoredQuery(context: Context?, query: String?) {
+	fun setStoredQuery(context: Context, query: String?) {
 		PreferenceManager.getDefaultSharedPreferences(context)
 			.edit {
 				putString(PREF_SEARCH_QUERY, query)
 			}
 	}
 
-	fun getLastResultId(context: Context?): String? {
+	fun getLastResultId(context: Context): String? {
 		return PreferenceManager.getDefaultSharedPreferences(context)
 			.getString(PREF_LAST_RESULT_ID, "")!!
 	}
 
-	fun setLastResultId(context: Context?, lastResultId: String?) {
+	fun setLastResultId(context: Context, lastResultId: String?) {
 		PreferenceManager.getDefaultSharedPreferences(context)
 			.edit {
 				putString(PREF_LAST_RESULT_ID, lastResultId)
 			}
 	}
 
-	fun isPolling(context: Context?): Boolean {
+	fun isPolling(context: Context): Boolean {
 		return PreferenceManager.getDefaultSharedPreferences(context)
 			.getBoolean(PREF_IS_POLLING, false)
 	}
 
-	fun setPolling(context: Context?, isOn: Boolean) {
+	fun setPolling(context: Context, isOn: Boolean) {
 		PreferenceManager.getDefaultSharedPreferences(context)
 			.edit()
 			.putBoolean(PREF_IS_POLLING, isOn)
